b6de79975f57e38327db4f80214fd8eb6555ce45
IsmaelAlbazi <ismaelalbazi@gmail.com>
2018-05-24 17:22:41 +0200

    setup dingen

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="icon" href="favicon.ico" type="image/x-icon"/>
+		<link rel="stylesheet" type="text/css" href="build/css/style.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+		<link rel="icon" type="image/png" sizes="16x16" href="build/img/backgrounds/favicon-16x16.png">
+		<title>Veritas Webdevelopment</title>
+	</head>
+	<body class="body">
+		<?php require_once'build/sections/header.php' ?>
+		<?php require_once'build/sections/home.php' ?>
+		<?php require_once'build/sections/footer.php' ?>
+		<!-- <script src="build/js/navigation.js"></script> -->
+	</body>
+</html>
---
+++ src/sections/footer.php
@@ -0,0 +1,3 @@
+<footer class="footer">
+	<span class="footer__span">je footer neef</span>
+</footer>
