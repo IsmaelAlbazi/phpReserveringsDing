671002a3e3d31fb45cbd6a4a69b03ee880b821f3
IsmaelAlbazi <ismaelalbazi@gmail.com>
2018-05-24 17:48:17 +0200

    Test

--- src/sections/header.php
+++
@@ -1,8 +0,0 @@
-<header class="header">
-	<img class="header__logo" src="" alt="">
-	<ul class="header__list">
-		<li class="header__list-item">home</li>
-		<li class="header__list-item">about</li>
-		<li class="header__list-item">contact</li>
-	</ul>
-</header>
