c1cb51080d76df710ab7393eb8efee64e6a44da0
jon <jgkerkhoven@gmail.com>
2018-05-25 21:30:42 +0200

    Remove whtespace

--- src/sections/booking.php
+++ src/sections/booking.php
@@ -1,16 +1,15 @@
 <section class="booking">
-	<form 
+	<form
 		class="booking__form"
-		action="src/php/reserveringOpslaan.php" 
+		action="src/php/reserveringOpslaan.php"
 		method="post"
 		id="booking-form"
 		name="booking-form"
 	>
-		<input class="booking__input booking__input--date" type="date" id="booking-date" name="datum" value="" required="" >
-		<input class="booking__input" type="time" id="booking-time" name="tijd" placeholder="08:30" required="" step="900" >
-
-		<input class="booking__input" required="" type="text" id="booking-name" name="naam" placeholder="Full Name" >
-		<select class="booking__input" id="booking-barber" name="kapper" value="Kies Kapper" >
+		<input class="booking__input booking__input--date" type="date" id="booking-date" name="datum" value="" required="">
+		<input class="booking__input" type="time" id="booking-time" name="tijd" placeholder="08:30" required="" step="900">
+		<input class="booking__input" required="" type="text" id="booking-name" name="naam" placeholder="Full Name">
+		<select class="booking__input" id="booking-barber" name="kapper" value="Kies Kapper">
 			<option value="Habib">Habib</option>
 			<option value="Saar">Fatima</option>
 			<option value="Hadiz">Hadiz</option>
