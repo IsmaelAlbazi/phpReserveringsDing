2825a283f5e96cb7f701a464af2e824b480fce8f
jon <jgkerkhoven@gmail.com>
2018-05-25 21:17:47 +0200

    Change order of html classes

--- src/sections/booking.php
+++ src/sections/booking.php
@@ -1,15 +1,15 @@
 <section class="booking">
 	<form 
+		class="booking__form"
 		action="src/php/reserveringOpslaan.php" 
 		method="post"
 		id="booking-form"
 		name="booking-form"
-		class="booking__form"
 	>
-		<input type="date" id="booking-date" name="datum" value="" class="booking__input booking__input--date">
-		<input type="time" id="booking-time" name="tijd" value="" class="booking__input">
-		<input type="text" id="booking-name" name="naam" value="Full Name" class="booking__input">
-		<select id="booking-barber" name="kapper" value="Kies Kapper" class="booking__input">
+		<input class="booking__input booking__input--date" type="date" id="booking-date" name="datum" value="">
+		<input class="booking__input" type="time" id="booking-time" name="tijd" value="">
+		<input class="booking__input" type="text" id="booking-name" name="naam" value="Full Name">
+		<select class="booking__input" id="booking-barber" name="kapper" value="Kies Kapper">
 			<option value="bob">bob</option>
 			<option value="saar">Saar</option>
 			<option value="frits">frits</option>
