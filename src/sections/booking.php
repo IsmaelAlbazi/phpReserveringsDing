7013fb5467d339a42b7fda7d6ab6adf4ae5a7017
IsmaelAlbazi <ismaelalbazi@gmail.com>
2018-05-25 21:31:16 +0200

    jon pushed

--- src/sections/booking.php
+++ src/sections/booking.php
@@ -1,18 +1,17 @@
 <section class="booking">
 	<form 
 		class="booking__form"
-		action="src/php/reserveringOpslaan.php" 
+		action="src/php/reserveringOpslaan.php"
 		method="post"
 		id="booking-form"
 		name="booking-form"
 	>
 		<input class="booking__input booking__input--date" type="date" id="booking-date" name="datum" value="" required="" >
 		<input class="booking__input" type="time" id="booking-time" name="tijd" placeholder="08:30" required="" step="900" >
-
 		<input class="booking__input" required="" type="text" id="booking-name" name="naam" placeholder="Full Name" >
 		<select class="booking__input" id="booking-barber" name="kapper" value="Kies Kapper" >
 			<option value="Habib">Habib</option>
-			<option value="Saar">Fatima</option>
+			<option value="Fatima">Fatima</option>
 			<option value="Hadiz">Hadiz</option>
 		</select>
 		<input class="booking__button" type="submit" value="Reserveren"></input>
